d8e64295fde4c1406703ea383fd3e36861449476
Frozen <zhangbaolin.1000@163.com>
2019-11-10 15:39:05 +0800

    manager 配置https访问

--- code-client/src/main/java/com/zbl/code/ClientApplication.java
+++ code-client/src/main/java/com/zbl/code/ClientApplication.java
@@ -23,6 +23,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @EnableCaching
 @ServletComponentScan
+//相同点: 都能够让注册中心发现, 扫描到该服务
+//不同点：@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
 @EnableEurekaClient
 @EnableFeignClients
 public class ClientApplication {
