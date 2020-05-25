220f74b4b6433373967f5ac7b2d68dd3782053c0
zbl5337 <zhangbaolin.1000@163.com>
2020-05-25 20:58:45 +0800

    session + redis 做session共享

---
+++ code-sso-session-redis/src/main/java/com/zbl/code/controller/DemoController.java
@@ -0,0 +1,28 @@
+package com.zbl.code.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: zbl
+ * @Date: 20:45 2020/5/25
+ * @Description:
+ */
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo01")
+    public String demo01(HttpServletRequest request) {
+        request.getSession().setAttribute("xijiao", "xo");
+        return "xxoo";
+    }
+
+    @GetMapping("/demo02")
+    public String demo02(HttpServletRequest request) {
+        return (String) request.getSession().getAttribute("xijiao");
+    }
+}
