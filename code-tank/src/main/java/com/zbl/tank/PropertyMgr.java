d6d592b15b814238b4ab28a05b2b579e53b3b7f9
zbl5337 <zhangbaolin.1000@163.com>
2020-05-27 16:24:55 +0800

    优化单例的property

--- code-tank/src/main/java/com/zbl/tank/PropertyMgr.java
+++ code-tank/src/main/java/com/zbl/tank/PropertyMgr.java
@@ -2,6 +2,7 @@ package com.zbl.tank;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.stream.IntStream;
 
 /**
  * @Author: zbl
@@ -11,22 +12,41 @@ import java.util.Properties;
  */
 public class PropertyMgr {
 
-    private static Properties properties = new Properties();
+    private static volatile PropertyMgr propertyMgr;
 
-    static {
-        try {
-            properties.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    private Properties properties = new Properties();
+
+    private PropertyMgr() {
+
+    }
+
+    public static PropertyMgr getInstance() {
+        if (propertyMgr == null) {
+            synchronized (PropertyMgr.class) {
+                if (propertyMgr == null) {
+                    try {
+                        propertyMgr = new PropertyMgr();
+                        propertyMgr.properties.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config.properties"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
+        return propertyMgr;
     }
 
     public static Object get(String key) {
-        return properties == null ? null : properties.get(key);
+        return getInstance().properties.get(key);
     }
 
     public static void main(String[] args) {
         System.out.println(get("gameHeight"));
         System.out.println(get("tankSpeed"));
+
+        IntStream.rangeClosed(1,100).forEach(i -> new Thread(() -> {
+            System.out.println(getInstance().properties.hashCode());
+        }).start());
+
     }
 }
