18bdc54606a08ee6951c8cf8259347f10a0ff411
zbl5337 <zhangbaolin.1000@163.com>
2020-06-16 19:28:31 +0800

    左神说算法01

---
+++ code-demo/src/main/java/com/zbl/arithmetic/class01/Code05_GetLessIndex.java
@@ -0,0 +1,36 @@
+package com.zbl.arithmetic.class01;
+
+/**
+ * @Author: zbl
+ * @Date: Created in 11:01 2020/6/15
+ * @Description: 局部最小
+ * @Version: $
+ */
+public class Code05_GetLessIndex {
+
+    public static int getLessIndex(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        if (arr.length == 1 || arr[0] < arr[1]) {
+            return 0;
+        }
+        if (arr[arr.length - 1] < arr[arr.length - 2]) {
+            return arr.length - 1;
+        }
+        int left = 1;
+        int right = arr.length - 2;
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (arr[mid] > arr[mid - 1]) {
+                right = mid - 1;
+            } else if (arr[mid] > arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return left;
+    }
+}
---
+++ code-demo/src/main/java/com/zbl/arithmetic/class01/selectionSort.java
@@ -0,0 +1,58 @@
+package com.zbl.arithmetic.class01;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @Author: zbl
+ * @Date: Created in 11:39 2020/5/28
+ * @Description: 选择排序
+ * @Version: $
+ */
+public class selectionSort {
+
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            //找到最小值的位置放前面
+            //其实就是索引为i的和数字最小的换位置
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+            System.out.println(Arrays.toString(arr));
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,5,4,3,2,0};
+        selectionSort(arr);
+//        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void test01() throws Exception {
+        //a = b, b = a
+        int a = 1;
+        int b = 2;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+
+}
