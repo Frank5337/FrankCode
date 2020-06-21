56cfbfc2f9e934b2759e2ca1921301f12ada3569
zbl5337 <zhangbaolin.1000@163.com>
2020-06-22 02:19:53 +0800

    web

---
+++ code-web/src/com/zbl/MyServlet.java
@@ -0,0 +1,32 @@
+package com.zbl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: zbl
+ * @Date: 18:58 2020/6/21
+ * @Description:
+ */
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("web project");
+        System.out.println("my web project");
+        //super.service(req, resp);
+    }
+}
