1246e4fbb1c5a9497a05a5766120d87f26b89ba5
zbl5337 <zhangbaolin.1000@163.com>
2020-07-18 20:22:46 +0800

    WebFlux

---
+++ code-webflux/src/main/java/com/zbl/code/pojo/Person.java
@@ -0,0 +1,29 @@
+package com.zbl.code.pojo;
+
+/**
+ * @Author: zbl
+ * @Date: 19:21 2020/7/18
+ * @Description:
+ */
+public class Person {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
