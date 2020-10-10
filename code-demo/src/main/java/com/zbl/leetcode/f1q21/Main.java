6eef14c5fb6116cfffdb5dadef47d45a14896232
zbl5337 <zhangbaolin.1000@163.com>
2020-10-10 15:15:05 +0800

    test

--- code-demo/src/main/java/com/zbl/leetcode/f1q21/Main.java
+++ code-demo/src/main/java/com/zbl/leetcode/f1q21/Main.java
@@ -2,7 +2,7 @@ package com.zbl.leetcode.f1q21;
 
 /**
  * @Author: zbl
- * @Date: Created in 10:50 2020/5/1
+ * @Date: Created in 10:50 2020/10/8
  * @Description:
  * @Version: $
  */
@@ -12,15 +12,36 @@ public class Main {
 
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        while (l1.next != null) {
-            while (l2.next != null) {
-                if (l1.val > l2.val) {
-
-                }
-            }
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while(cur!=null) {
+            //记录当前节点的下一个节点
+            tmp = cur.next;
+            //然后将当前节点指向pre
+            cur.next = pre;
+            //pre和cur节点都前进一位
+            pre = cur;
+            cur = tmp;
         }
-        return null;
+        return pre;
     }
+
+    /**
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 5->4->3->2->1->NULL
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if(head==null || head.next==null) {
+            return head;
+        }
+        ListNode cur = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return cur;
+    }
+
 }
---
+++ code-shiro/src/main/java/com/zbl/pojo/MyTest.java
@@ -0,0 +1,27 @@
+package com.zbl.pojo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: zbl
+ * @Date: Created in 2020/10/9
+ * @Description:
+ * @Version: $
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        User user = null;
+        if (user != null && user.getId().equals(1)) {
+            System.out.println(123);
+        }
+    }
+
+    @Test
+    public void test01() throws Exception {
+        Long a = 1L;
+        switch (1) {
+
+        }
+    }
+}
