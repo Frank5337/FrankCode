cac34c364a97ee2423d9a523343c474cb4116342
zbl5337 <zhangbaolin.1000@163.com>
2020-11-01 16:19:18 +0800

    zull

---
+++ code-zull/src/main/java/com/zbl/zull/ZullApplication.java
@@ -0,0 +1,13 @@
+package com.zbl.zull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZullApplication.class, args);
+    }
+
+}
---
+++ code-zull/src/test/java/com/zbl/zull/ZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbl.zull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
