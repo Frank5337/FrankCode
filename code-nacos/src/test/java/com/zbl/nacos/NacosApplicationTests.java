234b7bfd14c71e875433ef0d318d656283eb56d2
zbl5337 <zhangbaolin.1000@163.com>
2020-11-20 09:32:09 +0800

    nacos init

---
+++ code-nacos/src/test/java/com/zbl/nacos/NacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbl.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
