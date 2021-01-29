79ebd5bdc84e408cf726e6125818134aca0583b8
Frozen <zbl5337@gmail>
2021-01-29 21:21:09 +0800

    荷兰国旗v1

--- code-demo/src/main/java/com/zbl/arithmetic/basics/class03/Main.java
+++ code-demo/src/main/java/com/zbl/arithmetic/basics/class03/Main.java
@@ -14,22 +14,46 @@ public class Main {
 
     @Test
     public void test01() throws Exception {
-        int[] arr = new int[]{8,1,3,2,7,4,5};
+        int[] arr = new int[]{8, 1, 3, 2, 7, 4, 5,1,32,55,66,5};
         int num = 4;
-        System.out.println(Arrays.toString(arr));
-        partition(arr, num);
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
+        partition(arr, 0, 5);
+//        System.out.println(Arrays.toString(partition(arr, 0, 5)));
+//        System.out.println(Arrays.toString(arr));
     }
 
-    private void partition(int[] arr, int num) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= num) {
-                swap(arr, i, i-1);
+    /**
+     * 荷兰国旗第一版
+     * @param arr
+     * @param L
+     * @param num
+     * @return
+     */
+    private static int[] partition(int[] arr, int L, int num) {
+//        if (L > R) {
+//            return new int[]{-1, -1};
+//        }
+//        if (L == R) {
+//            return new int[]{L, R};
+//        }
+        int less = L - 1;
+        int more = arr.length - 1;
+        int index = L;
+        while (index < more) {
+            if (arr[index] == 5) {
+                index++;
+            } else if (arr[index] < 5) {
+                swap(arr, index++, ++less);
+            } else {
+                swap(arr, index, more--);
             }
+
         }
+        System.out.println(Arrays.toString(arr));
+        return new int[]{less + 1, more};
     }
 
-    private void swap(int[] arr, int a, int b ){
+    private static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
