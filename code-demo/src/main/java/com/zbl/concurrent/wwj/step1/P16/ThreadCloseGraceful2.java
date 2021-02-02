85a8c3cd59882f2f77fd952df7c9ab7ba73165bf
zbl5337 <zhangbaolin.1000@163.com>
2021-02-02 12:42:48 +0800

    优雅的关闭线程, 等价于过时的Thread.stop()

---
+++ code-demo/src/main/java/com/zbl/concurrent/wwj/step1/P16/ThreadCloseGraceful2.java
@@ -0,0 +1,44 @@
+package com.zbl.concurrent.wwj.step1.P16;
+
+/**
+ * @Author: zbl
+ * @Date: Created in 2021/2/1 12:24
+ * @Description:
+ * @Version: $
+ */
+public class ThreadCloseGraceful2 {
+
+    private static class Worker extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+//                try {
+//                    Thread.sleep(1_000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                    break;//return
+//                }
+                if (Thread.interrupted()) {
+                    break;
+                }
+            }
+
+            System.out.println("bbb");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        worker.start();
+
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        worker.interrupt();
+    }
+}
